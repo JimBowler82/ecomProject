e3855c6aabf6655857b9d245ddd5f7a28420eb7a
James Bowler <james.bowler@ecdltd.co.uk>
2021-04-22 16:25:36 +0100

    basic tables, product cards & pagination

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Route::get('/portal', function () {
     return view('portal');
