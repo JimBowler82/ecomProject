3368540cd6fd4a9e72657763f4598af36f2c91cd
James Bowler <james.bowler@ecdltd.co.uk>
2021-05-04 14:48:59 +0100

    move product edit script

--- public/js/edit-product.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const condition = {!! json_encode($product->condition) !!}
-    const options = document.querySelectorAll('#condition > option');
-    options.forEach(option => {
-        if (option.value === condition) {
-            option.selected = true
-        } 
-    })
-});
--- resources/views/product/edit-product.blade.php
+++ resources/views/product/edit-product.blade.php
@@ -116,6 +116,16 @@
         
     </div>
     @section('page-script')
-        <script src="{{ asset('js/edit-product.js') }}"></script>
+        <script >
+            window.addEventListener('DOMContentLoaded', () => {
+                const condition = {!! json_encode($product->condition) !!}
+                const options = document.querySelectorAll('#condition > option');
+                options.forEach(option => {
+                    if (option.value === condition) {
+                        option.selected = true
+                    } 
+                })
+            });
+        </script>
     @stop
 </x-app-layout>
