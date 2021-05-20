a31e68160dee6da44b5185a0e5f19ff4b36570e9
James Bowler <james.bowler@ecdltd.co.uk>
2021-05-20 18:06:13 +0100

    adjusting asset urls

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -2,7 +2,7 @@
 
 <div class='flex-1 bg-white flex flex-col items-center p-4 m-4 shadow-lg' style="max-width: 350px; min-width: 300px;">
     <div class='h-3/5 flex items-center justify-center'>
-        <img src="{{ asset($product->images->first()->location) }}" alt="" width="80%">
+        <img src="{{ asset('/storage/'.$product->images->first()->location) }}" alt="" width="80%">
     </div>
     <div class="flex flex-col flex-1 w-full ">
         <h3 class='font-bold mt-4 mb-2'>{{ $product->manufacturer }} <span class='font-normal'>{{ $product->model }}</span> </h3>
